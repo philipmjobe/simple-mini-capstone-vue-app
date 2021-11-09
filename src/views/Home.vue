da2ce115c0689946c542372fee422f7a38701dd8
Philip <philipmjobe@gmail.com>
2021-11-09 14:06:23 -0500

    added products in hard code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>
+      <u>{{ message }}</u>
+    </h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.price }}</p>
+    </div>
   </div>
 </template>
 
@@ -10,7 +16,11 @@
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to the end!",
+      products: [
+        { id: 1, name: "Product 1", price: 10 },
+        { id: 2, name: "Product 2", price: 230 },
+      ],
     };
   },
   created: function () {},
