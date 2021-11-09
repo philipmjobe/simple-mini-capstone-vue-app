9e17025dd414e54f306726b3131da60dd3d7efb3
Philip <philipmjobe@gmail.com>
2021-11-09 15:29:05 -0500

    added stuff i need to fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,11 @@
     <h1>
       <u>{{ message }}</u>
     </h1>
+    <h2>New Product</h2>
+    Name:
+    <input type="text" v-model="newProductParams.title" />
+    <h2>New Product</h2>
+    <button v-on:click="createPorduct()">Create Product</button>
     <div v-for="product in products" v-bind:key="product.id">
       <h3>{{ product.name }}</h3>
       <p>{{ product.price }}</p>
@@ -18,7 +23,7 @@ img {
 </style>
 
 <script>
-const axios = require("axios");
+import axios from "axios";
 export default {
   data: function () {
     return {
@@ -34,6 +39,24 @@ export default {
       axios.get("http://localhost:3000/products").then((response) => {
         this.products = response.data;
       });
+    },
+    createProduct: function () {
+      console.log("Creating a new product");
+
+      var params = {
+        name: "Test",
+        description: "Just a test",
+        price: 99,
+        image: "image.png",
+      };
+
+      axios
+        .post("http://localhost:3000/products", params)
+        .then((response) => {
+          console.log("Success", response.data);
+          this.product.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
     },
   },
 };
